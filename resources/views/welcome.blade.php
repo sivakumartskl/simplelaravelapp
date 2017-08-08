62531f3d0226d0ad2004270b6e3483417d0c51c3
sivakumartskl <sivakumartskl5a2@gmail.com>
2017-08-08 15:09:10 +0530

    Created signup and signin pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,61 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-        <title>Laravel</title>
+@section('title')
+    Welcome to Social networing app
+@endsection
 
-        <!-- Fonts -->
-       
-    </head>
-    <body>
-       <div>Hello world</div>
-    </body>
-</html>
+@section('content')
+    <div class="row">
+        <div class="col-sm-6">
+            <h3>Sign Up</h3>
+            <form class="form-horizontal" action="{{ url('signup') }}" method="post">
+              <div class="form-group"> 
+                <label class="control-label col-sm-2" for="email">Email </label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="control-label col-sm-2" for="firstName">FirstName </label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="firstName" placeholder="Enter first name" name="first_name">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="control-label col-sm-2" for="pwd">Password </label>
+                <div class="col-sm-10"> 
+                  <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password">
+                </div>
+              </div>
+              <div class="form-group"> 
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-primary">Sign Up</button>
+                </div>
+              </div>
+              <input type="hidden" name="_token" value="{{ Session::token() }}">
+            </form>
+        </div>
+        <div class="col-sm-6">
+            <h3>Sign In</h3>
+            <form class="form-horizontal" action="#" method="post">
+              <div class="form-group">
+                <label class="control-label col-sm-2" for="email">Email </label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="email" placeholder="Enter email" name="login_email">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="control-label col-sm-2" for="pwd">Password </label>
+                <div class="col-sm-10"> 
+                  <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="login_password">
+                </div>
+              </div>
+              <div class="form-group"> 
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-primary">Sign In</button>
+                </div>
+              </div>
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['web']], function () {
+	
+	Route::get('/', function () {
+	    return view('welcome');
+	});
+
+	Route::post('/signup', [
+		'uses' => 'UserController@postSignUp',
+		'as' => 'signup'
+	]);
+
+	Route::get('/dashboard', [
+		'uses' => 'UserController@getDashboard',
+		'as' => 'dashboard'
+	]);
+
 });
+
